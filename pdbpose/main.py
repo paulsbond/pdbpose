4068264fdb238f13ae914c55f80ab7f0cf8034b4
Paul Bond <git@paulsbond.co.uk>
2022-05-25 23:02:40 +0100

    Superposing on the first chain

--- pdbpose/main.py
+++ pdbpose/main.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from gemmi import Structure
+import gemmi
 from . import pdbe
 
 
@@ -12,22 +12,39 @@ def parse_args() -> Namespace:
 
 def main() -> None:
     args = parse_args()
+    reference = None
     for pdb_id, asym_id in sorted(pdbe.chains(args.uniprot)):
         print(pdb_id, asym_id)
         structure = pdbe.structure(pdb_id)
         trimmed = trim(structure, asym_id)
+        if reference is None:
+            reference = trimmed
+        else:
+            superpose(fixed=reference, movable=trimmed)
         write_pdb(trimmed, pdb_id, asym_id)
 
 
-def trim(structure: Structure, asym_id: str) -> Structure:
-    trimmed = Structure()
+def trim(structure: gemmi.Structure, asym_id: str) -> gemmi.Structure:
+    trimmed = gemmi.Structure()
     model = trimmed.find_or_add_model("1")
     chain = model.add_chain("A")
     chain.append_residues(structure[0].get_subchain(asym_id))
     return trimmed
 
 
-def write_pdb(structure: Structure, pdb_id: str, asym_id: str) -> None:
+def superpose(fixed: gemmi.Structure, movable: gemmi.Structure) -> None:
+    polymer1 = fixed[0].subchains()[0]
+    polymer2 = movable[0].subchains()[0]
+    polymer_type = gemmi.PolymerType.PeptideL
+    selection = gemmi.SupSelect.CaP
+    result = gemmi.calculate_superposition(polymer1, polymer2, polymer_type, selection)
+    for chain in movable[0]:
+        for residue in chain:
+            for atom in residue:
+                atom.pos = gemmi.Position(result.transform.apply(atom.pos))
+
+
+def write_pdb(structure: gemmi.Structure, pdb_id: str, asym_id: str) -> None:
     path = Path("pdbpose", f"{pdb_id}_{asym_id}.pdb")
     path.parent.mkdir(exist_ok=True)
     structure.write_minimal_pdb(str(path))
