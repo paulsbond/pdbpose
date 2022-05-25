fde39a06423a038675daed4542f74ca92da888dc
Paul Bond <git@paulsbond.co.uk>
2022-05-25 16:28:42 +0100

    Moved to a cli module

--- pdbpose/__main__.py
+++ pdbpose/__main__.py
@@ -1,17 +1,3 @@
-from argparse import ArgumentParser, Namespace
-from .pdbe import chains
-
-
-def parse_args() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument("uniprot")
-    return parser.parse_args()
-
-
-def main() -> None:
-    args = parse_args()
-    for chain in chains(args.uniprot):
-        print(chain)
-
+from .cli import main
 
 main()
