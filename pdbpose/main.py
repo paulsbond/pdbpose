3d3c5be5781994599a46308454f4f3272248cfdb
Paul Bond <git@paulsbond.co.uk>
2022-05-26 13:36:53 +0100

    Added a B chain with neighbours

--- pdbpose/main.py
+++ pdbpose/main.py
@@ -27,8 +27,36 @@ def main() -> None:
 def trim(structure: gemmi.Structure, asym_id: str) -> gemmi.Structure:
     trimmed = gemmi.Structure()
     model = trimmed.find_or_add_model("1")
-    chain = model.add_chain("A")
-    chain.append_residues(structure[0].get_subchain(asym_id))
+    model.add_chain("A")
+    model.add_chain("B")
+    search = gemmi.NeighborSearch(structure, max_radius=4.0).populate(include_h=False)
+    transforms = {}
+    for residue in structure[0].get_subchain(asym_id):
+        model["A"].add_residue(residue)
+        for atom in residue:
+            for mark in search.find_neighbors(atom, max_dist=4.0):
+                cra = mark.to_cra(structure[0])
+                transform = search.get_image_transformation(mark.image_idx)
+                image = structure.cell.find_nearest_pbc_image(
+                    atom.pos, cra.atom.pos, mark.image_idx
+                )
+                vec = transform.vec + gemmi.Vec3(*image.pbc_shift)
+                transform = gemmi.Transform(transform.mat, vec)
+                if cra.residue.subchain == asym_id and transform.is_identity():
+                    continue
+                added = transforms.setdefault((mark.chain_idx, mark.residue_idx), [])
+                if any(t.approx(transform, epsilon=1e-5) for t in added):
+                    continue
+                added.append(transform)
+                cloned_residue = cra.residue.clone()
+                num = model["B"][-1].seqid.num + 1 if len(model["B"]) > 0 else 1
+                cloned_residue.seqid = gemmi.SeqId(num, " ")
+                for cloned_atom in cloned_residue:
+                    fractional = structure.cell.fractionalize(cloned_atom.pos)
+                    transformed = gemmi.Fractional(*transform.apply(fractional))
+                    cloned_atom.pos = structure.cell.orthogonalize(transformed)
+                model["B"].add_residue(cloned_residue)
+    trimmed.remove_empty_chains()
     return trimmed
 
 
